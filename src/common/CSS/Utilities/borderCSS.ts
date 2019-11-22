1685980b3356c7b09f96ef92e5ba51fc2b198dc9
Michal <v25@vault25.net>
2019-11-22 17:19:15 +0100

    CSS added

--- src/common/CSS/Utilities/borderCSS.ts
+++ src/common/CSS/Utilities/borderCSS.ts
@@ -4,48 +4,125 @@ import GetColor from "../../colors/GetColor";
 export const borderCSS = css`
   border: 1px solid ${GetColor({ primary: true })};
 `;
+
+export const borderTopCSS = css`
+  border-top: 1px solid ${GetColor({ primary: true })};
+`;
+
+export const borderRightCSS = css`
+  border-right: 1px solid ${GetColor({ primary: true })};
+`;
+
+export const borderLeftCSS = css`
+  border-left: 1px solid ${GetColor({ primary: true })};
+`;
+
+export const borderBottomCSS = css`
+  border-bottom: 1px solid ${GetColor({ primary: true })};
+`;
+
+export const border0CSS = css`
+  border: 0 !important;
+`;
+
+export const borderTop0CSS = css`
+  border-top: 0 !important;
+`;
+
+export const borderRight0CSS = css`
+  border-right: 0 !important;
+`;
+
+export const borderLeft0CSS = css`
+  border-left: 0 !important;
+`;
+
+export const borderBottom0CSS = css`
+  border-bottom: 0 !important;
+`;
+
 export const borderDCSS = css`
-  border: 1px solid ${GetColor({ Dprimary: true })};
+  border-color ${GetColor({ Dprimary: true })}!important;
 `;
 export const borderLCSS = css`
-  border: 1px solid ${GetColor({ Lprimary: true })};
+  border-color ${GetColor({ Lprimary: true })}!important;
 `;
 export const borderAccentCSS = css`
-  border: 1px solid ${GetColor({ accent: true })};
+  border-color ${GetColor({ accent: true })}!important;
 `;
 export const borderSuccessCSS = css`
-  border: 1px solid ${GetColor({ success: true })};
+  border-color ${GetColor({ success: true })}!important;
 `;
 export const borderSuccessLCSS = css`
-  border: 1px solid ${GetColor({ successL: true })};
+  border-color ${GetColor({ successL: true })}!important;
 `;
 export const borderDangerCSS = css`
-  border: 1px solid ${GetColor({ danger: true })};
+  border-color ${GetColor({ danger: true })}!important;
 `;
 export const borderDangerLCSS = css`
-  border: 1px solid ${GetColor({ dangerL: true })};
+  border-color ${GetColor({ dangerL: true })}!important;
 `;
 export const borderInfoCSS = css`
-  border: 1px solid ${GetColor({ info: true })};
+  border-color ${GetColor({ info: true })}!important;
 `;
 export const borderInfoLCSS = css`
-  border: 1px solid ${GetColor({ infoL: true })};
+  border-color ${GetColor({ infoL: true })}!important;
 `;
 export const borderWarningCSS = css`
-  border: 1px solid ${GetColor({ warning: true })};
+  border-color ${GetColor({ warning: true })}!important;
 `;
 export const borderWarningLCSS = css`
-  border: 1px solid ${GetColor({ warningL: true })};
+  border-color ${GetColor({ warningL: true })}!important;
 `;
 export const borderDisabledCSS = css`
-  border: 1px solid ${GetColor({ Lgray: true })};
+  border-color ${GetColor({ Lgray: true })}!important;
 `;
 export const borderDisabledDCSS = css`
-  border: 1px solid ${GetColor({ Dgray: true })};
+  border-color ${GetColor({ Dgray: true })}!important;
 `;
 export const brdRoundedCSS = css`
   border-radius: 0.25rem;
 `;
+
+export const brdRoundedTopCSS = css`
+  border-top-left-radius: 0.25rem !important;
+  border-top-right-radius: 0.25rem !important;
+`;
+
+export const brdRoundedTop2xCSS = css`
+  border-top-left-radius: 0.5rem !important;
+  border-top-right-radius: 0.5rem !important;
+`;
+
+export const brdRoundedRightCSS = css`
+  border-top-right-radius: 0.25rem !important;
+  border-bottom-right-radius: 0.25rem !important;
+`;
+
+export const brdRoundedRight2xCSS = css`
+  border-top-right-radius: 0.5rem !important;
+  border-bottom-right-radius: 0.5rem !important;
+`;
+
+export const brdRoundedBottomCSS = css`
+  border-bottom-right-radius: 0.25rem !important;
+  border-bottom-left-radius: 0.25rem !important;
+`;
+
+export const brdRoundedBottom2xCSS = css`
+  border-bottom-right-radius: 0.5rem !important;
+  border-bottom-left-radius: 0.5rem !important;
+`;
+export const brdRoundedLeftCSS = css`
+  border-top-left-radius: 0.25rem !important;
+  border-bottom-left-radius: 0.25rem !important;
+`;
+
+export const brdRoundedLeft2xCSS = css`
+  border-top-left-radius: 0.5rem !important;
+  border-bottom-left-radius: 0.5rem !important;
+`;
+
 export const brdRounded2xCSS = css`
   border-radius: 0.5rem;
 `;
