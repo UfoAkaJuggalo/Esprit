736b49a634c5d9c8149f925eba44686c478886ca
Michal Nawrocki <v25@vault25.net>
2019-11-28 19:21:45 +0100

    AlignSelf refactored

--- src/common/CSS/Utilities/alignSelfCSS.ts
+++ src/common/CSS/Utilities/alignSelfCSS.ts
@@ -1,176 +1,222 @@
-import { css } from "@emotion/core";
+import { css, SerializedStyles } from "@emotion/core";
 import GetProperty from "../../config/GetProperty";
 
-export const alignSelfStartCSS = css`
-  -ms-flex-item-align: start !important;
-  align-self: flex-start !important;
-`;
-export const alignSelfStartSmCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+export interface Props {
+  mediaQuery?: keyof MediaQuery;
+}
+
+export const alignSelfStartCSS = (params: Props): SerializedStyles => {
+  const retCSS = css`
     -ms-flex-item-align: start !important;
     align-self: flex-start !important;
-  }
-`;
-export const alignSelfStartMdCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
-    -ms-flex-item-align: start !important;
-    align-self: flex-start !important;
-  }
-`;
-export const alignSelfStartLgCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
-    -ms-flex-item-align: start !important;
-    align-self: flex-start !important;
-  }
-`;
-export const alignSelfStartXlCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
-    -ms-flex-item-align: start !important;
-    align-self: flex-start !important;
-  }
-`;
+  `;
 
-export const alignSelfAutoCSS = css`
-  -ms-flex-item-align: auto !important;
-  align-self: auto !important;
-`;
-export const alignSelfAutoSmCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+  switch (params.mediaQuery) {
+    case "xs":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+          ${retCSS};
+        }
+      `;
+    case "sm":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
+          ${retCSS};
+        }
+      `;
+    case "md":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
+          ${retCSS};
+        }
+      `;
+    case "lg":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
+          ${retCSS};
+        }
+      `;
+    default:
+      return retCSS;
+  }
+};
+
+export const alignSelfAutoCSS = (params: Props): SerializedStyles => {
+  const retCSS = css`
     -ms-flex-item-align: auto !important;
     align-self: auto !important;
-  }
-`;
-export const alignSelfAutoMdCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
-    -ms-flex-item-align: auto !important;
-    align-self: auto !important;
-  }
-`;
-export const alignSelfAutoLgCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
-    -ms-flex-item-align: auto !important;
-    align-self: auto !important;
-  }
-`;
-export const alignSelfAutoXlCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
-    -ms-flex-item-align: auto !important;
-    align-self: auto !important;
-  }
-`;
+  `;
 
-export const alignSelfBaselineCSS = css`
-  -ms-flex-item-align: baseline !important;
-  align-self: baseline !important;
-`;
-export const alignSelfBaselineSmCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+  switch (params.mediaQuery) {
+    case "xs":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+          ${retCSS};
+        }
+      `;
+    case "sm":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
+          ${retCSS};
+        }
+      `;
+    case "md":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
+          ${retCSS};
+        }
+      `;
+    case "lg":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
+          ${retCSS};
+        }
+      `;
+    default:
+      return retCSS;
+  }
+};
+
+export const alignSelfBaselineCSS = (params: Props): SerializedStyles => {
+  const retCSS = css`
     -ms-flex-item-align: baseline !important;
     align-self: baseline !important;
-  }
-`;
-export const alignSelfBaselineMdCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
-    -ms-flex-item-align: baseline !important;
-    align-self: baseline !important;
-  }
-`;
-export const alignSelfBaselineLgCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
-    -ms-flex-item-align: baseline !important;
-    align-self: baseline !important;
-  }
-`;
-export const alignSelfBaselineXlCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
-    -ms-flex-item-align: baseline !important;
-    align-self: baseline !important;
-  }
-`;
+  `;
 
-export const alignSelfCenterCSS = css`
-  -ms-flex-item-align: center !important;
-  align-self: center !important;
-`;
-export const alignSelfCenterSmCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
-    -ms-flex-item-align: center !important;
-    align-self: center !important;
+  switch (params.mediaQuery) {
+    case "xs":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+          ${retCSS};
+        }
+      `;
+    case "sm":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
+          ${retCSS};
+        }
+      `;
+    case "md":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
+          ${retCSS};
+        }
+      `;
+    case "lg":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
+          ${retCSS};
+        }
+      `;
+    default:
+      return retCSS;
   }
-`;
-export const alignSelfCenterMdCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
-    -ms-flex-item-align: center !important;
-    align-self: center !important;
-  }
-`;
-export const alignSelfCenterLgCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
-    -ms-flex-item-align: center !important;
-    align-self: center !important;
-  }
-`;
-export const alignSelfCenterXlCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
-    -ms-flex-item-align: center !important;
-    align-self: center !important;
-  }
-`;
+};
 
-export const alignSelfEndCSS = css`
-  -ms-flex-item-align: flex-end !important;
-  align-self: flex-end !important;
-`;
-export const alignSelfEndSmCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
-    -ms-flex-item-align: flex-end !important;
-    align-self: flex-end !important;
-  }
-`;
-export const alignSelfEndMdCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
-    -ms-flex-item-align: flex-end !important;
-    align-self: flex-end !important;
-  }
-`;
-export const alignSelfEndLgCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
-    -ms-flex-item-align: flex-end !important;
-    align-self: flex-end !important;
-  }
-`;
-export const alignSelfEndXlCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
-    -ms-flex-item-align: flex-end !important;
-    align-self: flex-end !important;
-  }
-`;
+export const alignSelfCenterCSS = (params: Props): SerializedStyles => {
+  const retCSS = css`
+    -ms-flex-item-align: center !important;
+    align-self: center !important;
+  `;
 
-export const alignSelfStretchCSS = css`
-  -ms-flex-item-align: stretch !important;
-  align-self: stretch !important;
-`;
-export const alignSelfStretchSmCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+  switch (params.mediaQuery) {
+    case "xs":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+          ${retCSS};
+        }
+      `;
+    case "sm":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
+          ${retCSS};
+        }
+      `;
+    case "md":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
+          ${retCSS};
+        }
+      `;
+    case "lg":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
+          ${retCSS};
+        }
+      `;
+    default:
+      return retCSS;
+  }
+};
+
+export const alignSelfEndCSS = (params: Props): SerializedStyles => {
+  const retCSS = css`
+    -ms-flex-item-align: flex-end !important;
+    align-self: flex-end !important;
+  `;
+
+  switch (params.mediaQuery) {
+    case "xs":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+          ${retCSS};
+        }
+      `;
+    case "sm":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
+          ${retCSS};
+        }
+      `;
+    case "md":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
+          ${retCSS};
+        }
+      `;
+    case "lg":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
+          ${retCSS};
+        }
+      `;
+    default:
+      return retCSS;
+  }
+};
+
+export const alignSelfStretchCSS = (params: Props): SerializedStyles => {
+  const retCSS = css`
     -ms-flex-item-align: stretch !important;
     align-self: stretch !important;
+  `;
+
+  switch (params.mediaQuery) {
+    case "xs":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "xs" })}) {
+          ${retCSS};
+        }
+      `;
+    case "sm":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
+          ${retCSS};
+        }
+      `;
+    case "md":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
+          ${retCSS};
+        }
+      `;
+    case "lg":
+      return css`
+        @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
+          ${retCSS};
+        }
+      `;
+    default:
+      return retCSS;
   }
-`;
-export const alignSelfStretchMdCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "sm" })}) {
-    -ms-flex-item-align: stretch !important;
-    align-self: stretch !important;
-  }
-`;
-export const alignSelfStretchLgCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "md" })}) {
-    -ms-flex-item-align: stretch !important;
-    align-self: stretch !important;
-  }
-`;
-export const alignSelfStretchXlCSS = css`
-  @media (min-width: ${GetProperty({ mediaQuery: "lg" })}) {
-    -ms-flex-item-align: stretch !important;
-    align-self: stretch !important;
-  }
-`;
+};
--- src/common/CSS/UtilityResolver/alignSelfResolver.ts
+++ src/common/CSS/UtilityResolver/alignSelfResolver.ts
@@ -70,41 +70,57 @@ const AlignSelfResolver = (
 ): ArrayInterpolation<undefined> => {
   let css: ArrayInterpolation<undefined> = [];
 
-  if (params.alignSelfStart) css.push(alignSelfStartCSS);
-  if (params.alignSelfStartSm) css.push(alignSelfStartSmCSS);
-  if (params.alignSelfStartMd) css.push(alignSelfStartMdCSS);
-  if (params.alignSelfStartLg) css.push(alignSelfStartLgCSS);
-  if (params.alignSelfStartXl) css.push(alignSelfStartXlCSS);
+  if (params.alignSelfStart) css.push(alignSelfStartCSS({}));
+  if (params.alignSelfStartSm)
+    css.push(alignSelfStartCSS({ mediaQuery: "xs" }));
+  if (params.alignSelfStartMd)
+    css.push(alignSelfStartCSS({ mediaQuery: "sm" }));
+  if (params.alignSelfStartLg)
+    css.push(alignSelfStartCSS({ mediaQuery: "md" }));
+  if (params.alignSelfStartXl)
+    css.push(alignSelfStartCSS({ mediaQuery: "lg" }));
 
-  if (params.alignSelfAuto) css.push(alignSelfAutoCSS);
-  if (params.alignSelfAutoSm) css.push(alignSelfAutoSmCSS);
-  if (params.alignSelfAutoMd) css.push(alignSelfAutoMdCSS);
-  if (params.alignSelfAutoLg) css.push(alignSelfAutoLgCSS);
-  if (params.alignSelfAutoXl) css.push(alignSelfAutoXlCSS);
+  if (params.alignSelfAuto) css.push(alignSelfAutoCSS({}));
+  if (params.alignSelfAutoSm) css.push(alignSelfAutoCSS({ mediaQuery: "xs" }));
+  if (params.alignSelfAutoMd) css.push(alignSelfAutoCSS({ mediaQuery: "sm" }));
+  if (params.alignSelfAutoLg) css.push(alignSelfAutoCSS({ mediaQuery: "md" }));
+  if (params.alignSelfAutoXl) css.push(alignSelfAutoCSS({ mediaQuery: "lg" }));
 
-  if (params.alignSelfBaseline) css.push(alignSelfBaselineCSS);
-  if (params.alignSelfBaselineSm) css.push(alignSelfBaselineSmCSS);
-  if (params.alignSelfBaselineMd) css.push(alignSelfBaselineMdCSS);
-  if (params.alignSelfBaselineLg) css.push(alignSelfBaselineLgCSS);
-  if (params.alignSelfBaselineXl) css.push(alignSelfBaselineXlCSS);
+  if (params.alignSelfBaseline) css.push(alignSelfBaselineCSS({}));
+  if (params.alignSelfBaselineSm)
+    css.push(alignSelfBaselineCSS({ mediaQuery: "xs" }));
+  if (params.alignSelfBaselineMd)
+    css.push(alignSelfBaselineCSS({ mediaQuery: "sm" }));
+  if (params.alignSelfBaselineLg)
+    css.push(alignSelfBaselineCSS({ mediaQuery: "md" }));
+  if (params.alignSelfBaselineXl)
+    css.push(alignSelfBaselineCSS({ mediaQuery: "lg" }));
 
-  if (params.alignSelfCenter) css.push(alignSelfCenterCSS);
-  if (params.alignSelfCenterSm) css.push(alignSelfCenterSmCSS);
-  if (params.alignSelfCenterMd) css.push(alignSelfCenterMdCSS);
-  if (params.alignSelfCenterLg) css.push(alignSelfCenterLgCSS);
-  if (params.alignSelfCenterXl) css.push(alignSelfCenterXlCSS);
+  if (params.alignSelfCenter) css.push(alignSelfCenterCSS({}));
+  if (params.alignSelfCenterSm)
+    css.push(alignSelfCenterCSS({ mediaQuery: "xs" }));
+  if (params.alignSelfCenterMd)
+    css.push(alignSelfCenterCSS({ mediaQuery: "sm" }));
+  if (params.alignSelfCenterLg)
+    css.push(alignSelfCenterCSS({ mediaQuery: "md" }));
+  if (params.alignSelfCenterXl)
+    css.push(alignSelfCenterCSS({ mediaQuery: "lg" }));
 
-  if (params.alignSelfEnd) css.push(alignSelfEndCSS);
-  if (params.alignSelfEndSm) css.push(alignSelfEndSmCSS);
-  if (params.alignSelfEndMd) css.push(alignSelfEndMdCSS);
-  if (params.alignSelfEndLg) css.push(alignSelfEndLgCSS);
-  if (params.alignSelfEndXl) css.push(alignSelfEndXlCSS);
+  if (params.alignSelfEnd) css.push(alignSelfEndCSS({}));
+  if (params.alignSelfEndSm) css.push(alignSelfEndCSS({ mediaQuery: "xs" }));
+  if (params.alignSelfEndMd) css.push(alignSelfEndCSS({ mediaQuery: "sm" }));
+  if (params.alignSelfEndLg) css.push(alignSelfEndCSS({ mediaQuery: "md" }));
+  if (params.alignSelfEndXl) css.push(alignSelfEndCSS({ mediaQuery: "lg" }));
 
-  if (params.alignSelfStretch) css.push(alignSelfStretchCSS);
-  if (params.alignSelfStretchSm) css.push(alignSelfStretchSmCSS);
-  if (params.alignSelfStretchMd) css.push(alignSelfStretchMdCSS);
-  if (params.alignSelfStretchLg) css.push(alignSelfStretchLgCSS);
-  if (params.alignSelfStretchXl) css.push(alignSelfStretchXlCSS);
+  if (params.alignSelfStretch) css.push(alignSelfStretchCSS({}));
+  if (params.alignSelfStretchSm)
+    css.push(alignSelfStretchCSS({ mediaQuery: "xs" }));
+  if (params.alignSelfStretchMd)
+    css.push(alignSelfStretchCSS({ mediaQuery: "sm" }));
+  if (params.alignSelfStretchLg)
+    css.push(alignSelfStretchCSS({ mediaQuery: "md" }));
+  if (params.alignSelfStretchXl)
+    css.push(alignSelfStretchCSS({ mediaQuery: "lg" }));
 
   return css;
 };
