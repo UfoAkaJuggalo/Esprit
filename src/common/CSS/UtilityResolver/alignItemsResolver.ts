b3646ca731cd5dc68c82ea1b7ec0dc0576620dc9
Michal Nawrocki <v25@vault25.net>
2019-11-28 19:12:20 +0100

    AlignItems refactored

--- src/common/CSS/UtilityResolver/alignItemsResolver.ts
+++ src/common/CSS/UtilityResolver/alignItemsResolver.ts
@@ -1,21 +1,6 @@
 import { ArrayInterpolation } from "@emotion/core";
-import {
-  alignItemsStartCSS,
-  alignItemsCenterCSS,
-  alignItemsEndCSS,
-  alignItemsStartSmCSS,
-  alignItemsCenterSmCSS,
-  alignItemsEndSmCSS,
-  alignItemsStartMdCSS,
-  alignItemsCenterMdCSS,
-  alignItemsEndMdCSS,
-  alignItemsStartLgCSS,
-  alignItemsCenterLgCSS,
-  alignItemsEndLgCSS,
-  alignItemsStartXlCSS,
-  alignItemsCenterXlCSS,
-  alignItemsEndXlCSS
-} from "..";
+
+import { alignItemsCenterCSS, alignItemsEndCSS, alignItemsStartCSS } from "..";
 
 export interface AlignItemsProps {
   alignItemsStart?: boolean;
@@ -40,21 +25,31 @@ const AlignItemsResolver = (
 ): ArrayInterpolation<undefined> => {
   let css: ArrayInterpolation<undefined> = [];
 
-  if (params.alignItemsStart) css.push(alignItemsStartCSS);
-  if (params.alignItemsCenter) css.push(alignItemsCenterCSS);
-  if (params.alignItemsEnd) css.push(alignItemsEndCSS);
-  if (params.alignItemsStartSm) css.push(alignItemsStartSmCSS);
-  if (params.alignItemsCenterSm) css.push(alignItemsCenterSmCSS);
-  if (params.alignItemsEndSm) css.push(alignItemsEndSmCSS);
-  if (params.alignItemsStartMd) css.push(alignItemsStartMdCSS);
-  if (params.alignItemsCenterMd) css.push(alignItemsCenterMdCSS);
-  if (params.alignItemsEndMd) css.push(alignItemsEndMdCSS);
-  if (params.alignItemsStartLg) css.push(alignItemsStartLgCSS);
-  if (params.alignItemsCenterLg) css.push(alignItemsCenterLgCSS);
-  if (params.alignItemsEndLg) css.push(alignItemsEndLgCSS);
-  if (params.alignItemsStartXl) css.push(alignItemsStartXlCSS);
-  if (params.alignItemsCenterXl) css.push(alignItemsCenterXlCSS);
-  if (params.alignItemsEndXl) css.push(alignItemsEndXlCSS);
+  if (params.alignItemsStart) css.push(alignItemsStartCSS({}));
+  if (params.alignItemsStartSm)
+    css.push(alignItemsStartCSS({ mediaQuery: "xs" }));
+  if (params.alignItemsStartMd)
+    css.push(alignItemsStartCSS({ mediaQuery: "sm" }));
+  if (params.alignItemsStartLg)
+    css.push(alignItemsStartCSS({ mediaQuery: "md" }));
+  if (params.alignItemsStartXl)
+    css.push(alignItemsStartCSS({ mediaQuery: "lg" }));
+
+  if (params.alignItemsCenter) css.push(alignItemsCenterCSS({}));
+  if (params.alignItemsCenterSm)
+    css.push(alignItemsCenterCSS({ mediaQuery: "xs" }));
+  if (params.alignItemsCenterMd)
+    css.push(alignItemsCenterCSS({ mediaQuery: "sm" }));
+  if (params.alignItemsCenterLg)
+    css.push(alignItemsCenterCSS({ mediaQuery: "md" }));
+  if (params.alignItemsCenterXl)
+    css.push(alignItemsCenterCSS({ mediaQuery: "lg" }));
+
+  if (params.alignItemsEnd) css.push(alignItemsEndCSS({}));
+  if (params.alignItemsEndSm) css.push(alignItemsEndCSS({ mediaQuery: "xs" }));
+  if (params.alignItemsEndMd) css.push(alignItemsEndCSS({ mediaQuery: "sm" }));
+  if (params.alignItemsEndLg) css.push(alignItemsEndCSS({ mediaQuery: "md" }));
+  if (params.alignItemsEndXl) css.push(alignItemsEndCSS({ mediaQuery: "lg" }));
 
   return css;
 };
